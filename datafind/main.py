4a5b6bcc17c32f6622f73297d89ee259149af85e
Daniel Williams <daniel.williams@ligo.org>
2023-12-13 14:44:13 +0000

    Attempt to fix bug with cache file.

--- datafind/main.py
+++ datafind/main.py
@@ -261,6 +261,7 @@ def get_data_frames(detectors, start, end, duration):
         cache_string = ""
         for frame_file in files[detector]:
             cf = frame_file.split(".")[0].split("-")
+            frame_file = os.path.join("frames", frame_file)
             cache_string += f"{cf[0]}\t{cf[1]}\t{cf[2]}\t{cf[3]}\tfile://localhost{os.path.abspath(frame_file)}\n"
         with open(os.path.join("cache", f"{detector}.cache"), "w") as cache_file:
             cache_file.write(cache_string)
