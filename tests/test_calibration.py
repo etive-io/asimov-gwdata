8944ed92b686ee6c77fb58123b0d52fc08e567ea
Daniel Williams <daniel.williams@ligo.org>
2025-06-02 18:19:46 +0100

    Added V1 test file.

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -10,7 +10,7 @@ class CalibrationDataTests(unittest.TestCase):
     package will correctly identify calibration files
     in the file structure which is provided to it.
     """
-    
+
     @patch('glob.glob')
     def test_lookup(self, mock_glob):
         """Test to check that the nearest uncertainty file is correctly identified."""
@@ -20,10 +20,10 @@ class CalibrationDataTests(unittest.TestCase):
             "/home/cal/public_html/archive/H1/uncertainty/1370/242226/calibration_uncertainty_H1_1370242228.txt"
         ]
         mock_glob.return_value = file_list
-        
+
         output = get_o4_style_calibration(dir="test", time=1370242226.4)
 
-        self.assertEqual(output['L1'], file_list[1])
+        self.assertEqual(output.get('L1', None), None)
         self.assertEqual(output['H1'], file_list[1])
 
     @patch('glob.glob')
@@ -41,7 +41,7 @@ class CalibrationDataTests(unittest.TestCase):
         ]
 
         mock_glob.return_value = file_list
-        
+
         output = get_o4_style_calibration(dir="test", time=1370242226.4)
         self.assertEqual(output['H1'], file_list[1])
         self.assertEqual(output['L1'], file_list[-1])
@@ -49,10 +49,10 @@ class CalibrationDataTests(unittest.TestCase):
 class VirgoCalibration(unittest.TestCase):
     """Test Virgo-style calibration uncertainty, distributed in frames."""
 
-    
+
     def setUp(self):
         """Create a calibration envelope object"""
-        self.test_frame = "tests/test_data/V-HoftAR1-1397154000-2000.gwf"
+        self.test_frame = "tests/test_data/V1.gwf"
 
         self.envelope = CalibrationUncertaintyEnvelope(frame=self.test_frame)
 
