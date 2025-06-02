125430ab45b8c588d9e10973048059042dc75c5e
Daniel Williams <daniel.williams@ligo.org>
2025-06-02 18:42:15 +0100

    Update another test.

--- tests/test_frames.py
+++ tests/test_frames.py
@@ -29,12 +29,15 @@ class TestLIGOFrames(unittest.TestCase):
 
         mock_shutil.return_value = True
 
-        urls, files = datafind.frames.get_data_frames_private(
-            types=["H1:H1_HOFT_C02",
-                   "L1:L1_HOFT_C02"],
-            start=1126259460,
-            end=1126259464,
-            download=True,
-        )
+        try:
+            urls, files = datafind.frames.get_data_frames_private(
+                types=["H1:H1_HOFT_C02",
+                    "L1:L1_HOFT_C02"],
+                start=1126259460,
+                end=1126259464,
+                download=True,
+            )
 
-        self.assertEqual(files['H1'], 'H-H1_HOFT_C02-1126256640-4096.gwf')
+            self.assertEqual(files['H1'], 'H-H1_HOFT_C02-1126256640-4096.gwf')
+        except requests.exceptions.HTTPError:
+            self.skipTest("Cannot access gw_data_find.")
